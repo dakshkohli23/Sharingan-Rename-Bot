29a3fe2a18a7a1d4ad8d53c87651a0624d7d0aac
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-21 15:05:35 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,5 +27,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(861055237)
+    Config.AUTH_USERS.add(677682427)
     app.run()
