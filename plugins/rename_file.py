aab3b35052b9fc15dbdb36060bbf71719e84698c
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-21 14:56:31 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -23,7 +23,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import filters 
-from pyrogram import Client as Mai_bOTs
+from pyrogram import Client as Compass_Botz
 
 #from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
@@ -38,7 +38,7 @@ from PIL import Image
 from database.database import *
 
 
-@Mai_bOTs.on_message(pyrogram.filters.command(["rename"]))
+@Compass_Botz.on_message(pyrogram.filters.command(["rename"]))
 async def rename_doc(bot, update):
     update_channel = Config.UPDATE_CHANNEL
     if update_channel:
