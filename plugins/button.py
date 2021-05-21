fef4ee09b2883d7f9e7288e0f3bb464c0d2ecdc0
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-21 14:50:42 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -1,9 +1,9 @@
 import pyrogram
 
 from plugins.Zee5_dl import zee5_execute
-from pyrogram import Client as Mai_bOTs
+from pyrogram import Client as Compass_Botz
 
-@Mai_bOTs.on_callback_query()
+@Compass_Botz.on_callback_query()
 async def formatbuttons(bot, update):
        
     if "|" in update.data:
