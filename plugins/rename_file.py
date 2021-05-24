66d36b40949a685e73321959e753f9d605acf58d
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-24 13:30:07 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -16,7 +16,7 @@ if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
     from config import Config
-from sample_config import LOG_CHANNEL
+
 # the Strings used for this "thing"
 from translation import Translation
 
