b58c940852ada3d7d8a8016711e6fec60cc3ffd4
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-06 14:58:13 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -79,6 +79,50 @@ async def help_user(bot, update):
         )
     )       
 
+
+@Compass_Botz.on_message(pyrogram.filters.command(["start"]))
+async def text(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await update.reply_text("You are Banned")
+        return
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text(" Sorry, You are **B A N N E D**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+            await update.reply_text(
+                text="**Please Join My Update Channel Before Using Me..**",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+            return
+        else:
+            await update.reply_text(Translation.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup(
+              [
+                [
+                        InlineKeyboardButton("❗ Help", callback_data = "ghelp")
+                ],
+                [
+                    InlineKeyboardButton('Support Channel', url='https://t.me/Compass_Botz'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/Dlaize')
+                ],
+                [
+                    InlineKeyboardButton('Anime Index', url='https://t.me/Cartoon_seriesz'),
+                    InlineKeyboardButton('Source', url='https://github.com/dakshkohli23/Sharingan-Rename-Bot')
+                ]
+            ]
+        ),
+        reply_to_message_id=update.message_id
+    )
+            return 
+
+            
 @Compass_Botz.on_callback_query()
 async def cb_handler(client: Compass_Botz , query: CallbackQuery):
     data = query.data
