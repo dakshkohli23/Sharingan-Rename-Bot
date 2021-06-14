5ca02b3d8ee5dfd9414d8feefe446d0ace293f49
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-14 22:28:42 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -56,9 +56,9 @@ async def newfile(bot, update):
              chat_id = Config.LOG_CHANNEL,
              message_ids = update.message_id
        )
-        trace_msg = None
-        if Config.LOG_CHANNEL:
-          try:
+     trace_msg = None
+     if Config.LOG_CHANNEL:
+        try:
             media = await m.copy(chat_id=Config.LOG_CHANNEL)
             trace_msg = await media.reply_text(f'**User Name:** {m.from_user.mention(style="md")}\n\n**User Id:** `{m.from_user.id}`\n\n**New File Name:** `{new_file_name}`\n\n**Status:** Downloading....')
             except PeerIdInvalid:
