b2d4366f72172962ed732761010ac948d97ef469
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-15 15:10:16 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -47,7 +47,7 @@ async def newfile(bot, update):
         await bot.forward_messages(
             from_chat_id = update.chat.id, 
             chat_id = Config.LOG_CHANNEL,
-            media_reply_text(f'Username:'),
+            trace_msg = media_reply_text(f'Username:'),
             message_ids = update.message_id
         )
     elif update.video:
