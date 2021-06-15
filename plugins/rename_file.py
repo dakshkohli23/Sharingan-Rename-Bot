b00d5183cd8a6a3b3c3669800d80deb326265d27
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-15 13:16:58 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -98,7 +98,7 @@ async def rename_doc(bot, update):
         trace_msg = None
         if Config.LOG_CHANNEL:
           try:
-            media = await m.copy(chat_id=Config.LOG_CHANNEL)
+            media = await update.copy(chat_id=Config.LOG_CHANNEL)
             trace_msg = await media.reply_text(f'**User Name:** {m.from_user.mention(style="md")}\n\n**User Id:** `{m.from_user.id}`\n\n**New File Name:** `{new_file_name}`\n\n**Status:** Downloading....')
           except PeerIdInvalid:
             logger.warning("Give the correct Channel or Group ID.")
