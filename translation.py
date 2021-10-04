360425f283cb6b185bbea651a3cea60d1e28b01d
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-04 14:04:09 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,32 +1,37 @@
 class Translation(object):
-    START_TEXT = """<b>Hey! {}\n
-I'm Renamer + File To Video Converter Bot With Permanent Thumbnail Support!⚡</b>
+    START_TEXT = """Hey {}
 
-<b>Do /help for more Details ...</b>
+Welcome to {}
+
+I am Fastest Renamer Bot which help you to rename and convert files in less time. Use /help command to see features.
+
+Just send a File or Media to get started
+
+By @Tellybots_4u
 """
 
-    RENAME_403_ERR = "What Are You Doing? Bruuh!!"
-    BANNED_USER_TEXT = "Sorry!! But Mr. Dlaize Banned You From Using This Service. That Means You Can't Run ME Now! \n \n Contact : @Dlaize For more Details.. " 
+    RENAME_403_ERR = "What Are You Doing? Bruhh!!"
+    BANNED_USER_TEXT = "Sorry!! But Tellybots.4u Banned You From Using This Service. That Means You Can't Run ME Now! \n \n Contact : @Tellybots_support For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do, Mate?"
-    UPGRADE_TEXT = "CONTACT : Mr. @Dlaize"
-    DOWNLOAD_START = "<b>╭─ ⌊ 📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
-    UPLOAD_START = "<b>╭─ ⌊ 📤 ᴜᴘʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
+    UPGRADE_TEXT = "CONTACT : support group. @Tellybots_support"
+    DOWNLOAD_START = "<b> 📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
+    UPLOAD_START = "<b> 📤 ᴜᴘʟᴏᴀᴅɪɴɢ ⌉ \n│\n</b>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for Using Me SHARE > ⚡</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT : <a href='https://telegram.me/dlaize'> ᴍʀ. ᴅʟᴀɪᴢᴇ</a>"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact Mr. @Dlaize"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "If you got this message you should contact @tellybots_support"
     SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ This Is Permanent Until</b> ❌ /delthumb ❌"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared Succesfully🤦"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail Cleared Succesfully"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared Succesfully."
-    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
+    SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully </b>"
     REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename file name extension To rename a file</b>"
     REPLY_TO_FILE_FOR_CONVERT = "<b> Please Reply To An File With /c2v To Convert It Into Streamable Video File</b>"
     REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_THUMB_FOUND = "No Thumbnail found ☠️"
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters 🔻🔻"""
+    IFLONG_FILE_NAME = """Files name is too large...Decrease The number Of Letters 🔻🔻"""
     ABOUT_ME = """<b><u>About Me:</u></b>
     
 <b>An Multi purpose Bot Which Can :-</b>
@@ -34,11 +39,11 @@ I'm Renamer + File To Video Converter Bot With Permanent Thumbnail Support!⚡</
 ⚡ Set Custom Thumbnail
 ⚡ Convert Files into Video 
 
-📱 Suggestion & Feedback ➜ <a href="https://telegram.dog/dlaize"><b>Mr. Dlaize</b></a>
+Support group 🗯️ ➜ <a href="https://telegram.dog/dlaize"><b>Tellybits_4u</b></a>
     """
-    HELP_USER = """🚨 Oh! You Need Help! Choose Available Commands below!
-───────────────
-<b>Made with Thunder ⚡ by : @Dlaize</b>
+    HELP_USER = """Use below text whichever you want to know to use !
+
+<b>Made with 💕 by : @Tellybots4u</b>
 """
     
     RENAME_HELP = """
@@ -57,4 +62,4 @@ I'm Renamer + File To Video Converter Bot With Permanent Thumbnail Support!⚡</
 ➠ Send '/delthumb' to Deleting Thumbnail.
 ➠ Send '/showthumb' for view Current Thumbnail.
 """
-CCAPTION_HELP = """Here Are The Available Commands In Custom Caption \n\n\n ▪️<code>/scaption</code> Use This Command To Save Your Custom Caption \n<b>Usage:</b> <code>/scaption your caption text</code> \n\n<b>[You Can Use</b> <code>{filename}</code> <b>For showing new file name in the caption]</b> """
+CCAPTION_HELP = """Here Are The Available Commands In Custom Caption \n\n\n 🚦<code>/scaption</code> Use This Command To Save Your Custom Caption \n<b>Usage:</b> <code>/scaption your caption text</code> \n\n<b>[You Can Use</b> <code>{filename}</code> <b>For showing new file name in the caption]</b> """
